4af4023aca1fcdb273e5f09e5bf15cf6041224d8
Baptiste <baptiste.rebillard@outlook.fr>
2023-02-01 14:13:01 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <GL/freeglut.h>
+void affiche()
+{
+glClear(GL_COLOR_BUFFER_BIT);
+glColor3f(1.0, 0.0, 0.0);
+glBegin(GL_POLYGON);
+glVertex3f(0.0, 0.0, 0.0);
+glVertex3f(0.0, 1.0, 0.0);
+glVertex3f(1.0, 1.0, 0.0);
+glVertex3f(0.0, 0.0, 0.0);
+glEnd();
+glFlush();
+}
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode(GLUT_SINGLE);
+glutInitWindowSize(300, 200);
+glutInitWindowPosition(100, 100);
+glutCreateWindow("OpenGL-Test2");
+glutDisplayFunc(affiche);
+glutMainLoop();
+return 0;
+}
