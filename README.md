8ebd87ad1240f1346cee1b70f46ed071d610f796
Leandro-rdz <leandro.rdz604@gmail.com>
2023-02-15 01:51:01 +0100

    update main: 2 3 idees

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # real-intgine
 to int games à l'infini !
+
+https://en.wikipedia.org/wiki/3D_projection
--- sources/js/main.js
+++ sources/js/main.js
@@ -2,20 +2,20 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
 
-ctx.beginPath();
+/*ctx.beginPath();
 ctx.moveTo(0, 0, 0);
 ctx.lineTo(20, 40, 50);
 ctx.fillStyle = "#FF0000";
 ctx.stroke();
 ctx.closePath();
-
+*/
 
 //alert(3)
-//s ? c = camera
-function TransformedPoint(sx, sy, sz, cx, cy, cz) {
+//s point a transfo c = camera a = ecran
+function TransformedPoint(sx, sy, sz, cx, cy, cz , ax, ay, az) {
 	dx = cy*(sz*(ay-cy)+cz*(ax-cx))-sy*(az-cz)
-	dy = sx*(cy*(az-cz)+sy*(sz*y+cz*(ax-cx)))+cx*(cz*(ay-cy)-sz*(ax-cx))
-	dz = cx*(cy*(az-cz)+sy*(sz*y+cz*(ax-cx)))-sx*(cz*(ay-cy)-sz*(ax-cx))
+	dy = sx*(cy*(az-cz)+sy*(sz*(ay-cy)+cz*(ax-cx)))+cx*(cz*(ay-cy)-sz*(ax-cx))
+	dz = cx*(cy*(az-cz)+sy*(sz*(ay-cy)+cz*(ax-cx)))-sx*(cz*(ay-cy)-sz*(ax-cx))
 
 	return [dx, dy, dz]
 }
@@ -26,11 +26,12 @@ function Project(dx, dy, dz, ex, ey, ez) {
 
 
 ctx.beginPath();
+ctx.lineWidth = 5;
 
-tp = TransformedPoint(20, 20, 20, 0, 0, 20)
-ctx.moveTo(Project(tp[0], tp[1], tp[2], 1, 1, 0));
-
+tp = TransformedPoint(10, 10, 10, 0, 0, 10, 0, 10, 10)
+ctx.moveTo(Project(tp[0], tp[1], tp[2], 1, 1, 10)[0], Project(tp[0], tp[1], tp[2], 1, 1, 10)[1]);
+	
 tp = TransformedPoint(40, 40, 40, 0, 0, 0)
-ctx.moveTo(Project(tp[0], tp[1], tp[2] 1, 1, 0));
-//ctx.strokeStyle = "Red";
+ctx.lineTo(Project(tp[0], tp[1], tp[2], 1, 1, 0)[0], Project(tp[0], tp[1], tp[2], 1, 1, 0)[1]);
+ctx.strokeStyle = "Red";
 ctx.stroke();
