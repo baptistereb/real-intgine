59532e8cf72bca3b5f39abcbc7f7d589d5468596
Baptiste <baptiste.rebillard@outlook.fr>
2023-02-21 00:50:58 +0100

    test: fleche

--- sources/js/main.js
+++ sources/js/main.js
@@ -7,19 +7,30 @@ SetDisplay(300, 300, 300)
 
 
 document.addEventListener("keydown",keyPush);
-setInterval(mainloop,20);
+setInterval(mainloop,1000/15);
 
 i=0
 function mainloop() {
 	i = i+1
-	SetCamera(0, 0, i)
+    ctx.fillStyle="white";
+    ctx.fillRect(0,0,canvas.width,canvas.height);
+	SetCamera(0, 0, 0)
 	DrawTriangle([10, 2, 10], [2, 5, 1], [50, 0, 0], "red")
 }
 
 function keyPush(evt) {
     switch(evt.keyCode) {
-        case 37:
-            alert("ok")
+        case 37: // <-
+        	camera_angle.x = camera_angle.x - 10
+            break;
+        case 38: // haut
+        	camera_angle.y = camera_angle.y + 10
+            break;
+        case 39: // ->
+        	camera_angle.x = camera_angle.x + 10
+            break;
+        case 40: // bas
+        	camera_angle.y = camera_angle.y - 10
             break;
     }
 }
