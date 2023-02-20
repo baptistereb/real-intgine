89e4c163a8e8ea350cc7c0efd85e47eb66886f44
Baptiste <baptiste.rebillard@outlook.fr>
2023-02-21 00:57:52 +0100

    fix: pas de 0.1 via les fleches

--- sources/js/main.js
+++ sources/js/main.js
@@ -21,16 +21,16 @@ function mainloop() {
 function keyPush(evt) {
     switch(evt.keyCode) {
         case 37: // <-
-        	camera_angle.x = camera_angle.x - 1
+        	camera_angle.x = camera_angle.x - 0.01
             break;
         case 38: // haut
-        	camera_angle.y = camera_angle.y + 1
+        	camera_angle.y = camera_angle.y + 0.01
             break;
         case 39: // ->
-        	camera_angle.x = camera_angle.x + 1
+        	camera_angle.x = camera_angle.x + 0.01
             break;
         case 40: // bas
-        	camera_angle.y = camera_angle.y - 1
+        	camera_angle.y = camera_angle.y - 0.01
             break;
     }
 }
