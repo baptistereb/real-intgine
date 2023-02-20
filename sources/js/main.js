91526f93f5bb588da2f93b701c2034005fa6444e
Baptiste <baptiste.rebillard@outlook.fr>
2023-02-21 00:42:03 +0100

    test: boucle

--- sources/js/main.js
+++ sources/js/main.js
@@ -1,8 +1,25 @@
 var canvas = document.getElementById("Canvas");
 var ctx = canvas.getContext("2d");
 
-SetCamera(1, 5, 10)
+SetCamera(0, 0, 0)
 SetCameraAngle(5, 5, 5)
-SetDisplay(10, 10, 10)
+SetDisplay(300, 300, 300)
 
-DrawTriangle([10, 2, 10], [2, 5, 1], [50, 0, 0], "red")
+
+document.addEventListener("keydown",keyPush);
+setInterval(mainloop,20);
+
+i=0
+function mainloop() {
+	i = i+1
+	SetCamera(0, 0, i)
+	DrawTriangle([10, 2, 10], [2, 5, 1], [50, 0, 0], "red")
+}
+
+function keyPush(evt) {
+    switch(evt.keyCode) {
+        case 37:
+            alert("ok")
+            break;
+    }
+}
