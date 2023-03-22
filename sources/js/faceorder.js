82e148f510b88cc458281ec076565c291032b087
Leandro-rdz <leandro.rdz604@gmail.com>
2023-03-22 11:36:55 +0100

    update faceorder

--- sources/js/faceorder.js
+++ sources/js/faceorder.js
@@ -1,30 +1,8 @@
-function faceorder (cama, dis, fac1, fac2, fac3,fac4) {
-
+function faceorder (cama, dis, faces) {
 	NewDisplay(dis,cama)
-	f1= fac1[1] - base.y
-	f2= fac2[1] - base.y
-	f3= fac3[1] - base.y
-	f4= fac4[1] - base.y
-	/*if f1 <= f2{
-		if f2<=f3{
-			if f3<=f4{
-				DrawTriangle(fac4[0],fac4[1],fac4[2], "pink")
-				DrawTriangle(fac3[0],fac3[1],fac3[2], "red")
-				DrawTriangle(fac2[0],fac2[1],fac2[2], "blue")
-				DrawTriangle(fac1[0],fac1[1],fac1[2], "green")	
-			
-			}		
-		}
-
-	}	
-	else{
-		if f1<=f3{
-			if f3<=f4{
-				DrawTriangle(fac4[0],fac4[1],fac4[2], "pink")
-				DrawTriangle(fac3[0],fac3[1],fac3[2], "red")
-				DrawTriangle(fac1[0],fac1[1],fac1[2], "green")	
-				DrawTriangle(fac2[0],fac2[1],fac2[2], "blue")
-	
+	var l=[]
+	for (const i of faces) {
+		l.append(faces[i][1] - base.y)
 	}
-*/
+	return  l..reverse.sort
 }
