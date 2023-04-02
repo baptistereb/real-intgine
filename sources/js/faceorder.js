5e1bbf74433fb63bb838fdb862a8e5372c68b8c0
Baptiste <baptiste.rebillard@outlook.fr>
2023-04-02 11:58:31 +0200

    feat(project): centre des faces
    Dans l'optique de les intégrer plus tard à une fonction pour afficher les faces dans l'ordre

--- sources/js/faceorder.js
+++ sources/js/faceorder.js
@@ -1,3 +1,19 @@
+function centerOfTriangle(p1, p2, p3) {
+	return [
+		(p1[0]+p2[0]+p3[0])/3,
+		(p1[1]+p2[1]+p3[1])/3,
+		(p1[2]+p2[2]+p3[2])/3
+	]
+}
+
+function centerOfSquare(p1, p2, p3, p4) {
+	return [
+		(p1[0]+p2[0]+p3[0]+p4[0])/4,
+		(p1[1]+p2[1]+p3[1]+p4[1])/4,
+		(p1[2]+p2[2]+p3[2]+p4[2])/4
+	]
+}
+
 function faceOrder (cama, dis, faces) {
 	//NewDisplay(dis,cama)
 	let l=[]
