ed9d9e25f22622834e2c9d08693d12a31dc936f8
Baptiste <baptiste.rebillard@outlook.fr>
2023-04-03 23:50:22 +0200

    fix: parsing

--- sources/js/file.js
+++ sources/js/file.js
@@ -1,18 +1,19 @@
 function strToMatrix(str) {
-    matrix = str.split("\n").map(line => line.split(" "));
+    matrix = str.split("\n").map(line => line.split(" "))
+    returnm = []
     for(let i=0; i<matrix.length; i++) {
-        if(matrix[i][0] != "f") {
-            matrix.splice(i,1)
+        if(matrix[i][0] == 'f') {
+            matrix[i][3] = matrix[i][3].slice(0, -1)
+            returnm.push(matrix[i])
         }
     }
-    return matrix
+    return returnm
 }
 
 document.getElementById('inputFile').addEventListener('change', function() {
 var file = new FileReader();
 file.onload = () => {
-  //document.getElementById('output').textContent = file.result;
-  console.log(file.result)
+  //console.log(file.result)
   console.log(strToMatrix(file.result))
 }
 file.readAsText(this.files[0])
