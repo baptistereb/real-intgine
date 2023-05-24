e2aa3554d0a18616bb46f800a34049072ba15885
Baptiste <baptiste.rebillard@outlook.fr>
2023-05-24 12:42:25 +0200

    fix: bug qui empechais toutes les faces de s'afficher

--- sources/js/facemanager.js
+++ sources/js/facemanager.js
@@ -1,12 +1,11 @@
 function AddTriangle(co1, co2, co3, color) {
 	triangle_list.push([co1, co2, co3, color, 0])	//on ajoute une face à la liste des faces à afficher
-													//le 0 correspond a la distance qui sera ensuite modifié
-}
+}													//le 0 correspond a la distance qui sera ensuite modifié
 
 function DrawAllTriangle() {
 	triangle_list = faceOrder(triangle_list)
-	triangle_list.forEach(function(element) {
-		face = triangle_list.shift() 					//on prend la premiere face de la list et on la retire
+	while((face = triangle_list.shift()) !== undefined) {
 		DrawTriangle(face[0],face[1],face[2],face[3])	//on draw la face retiré
-	})
+	}
+	triangle_list=[]
 }
